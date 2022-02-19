3a640c43e86fd9c8ff255285e83503bd181a9ed6
khumn010 <natasha.khumalo@vcontractor.co.za>
2022-02-19 13:10:07 +0200

    Added file

---
+++ SQLQuery1.sql
@@ -0,0 +1,22 @@
+--CREATING VIEWS--
+
+CREATE VIEW vMovies
+AS
+SELECT [MovieName],
+		[ReleaseDate],
+		[RunningTime],
+		[MovieDescription],
+		[Genre]
+FROM Movies
+GO
+
+--CREATE STORED PROCEDURE--
+
+CREATE PROCEDURE [dbo].[uspGetActorInfo]
+
+@MovieID int.
+@WriterID int = NULL
+
+AS
+	SELECT m.MovieName,
+	m.Release,
