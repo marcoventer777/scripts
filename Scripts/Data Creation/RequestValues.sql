02d1a460b7f66fc747ba0efb4300f89561d3c183
Mpho Futjane <mpho.futjane@vumatel.co.za>
2022-02-23 11:14:42 +0200

    Added fake data creation scripts for Location Codes, Location Values, and Request Values

---
+++ Scripts/Data Creation/RequestValues.sql
@@ -0,0 +1,20 @@
+USE DonoraDB
+GO
+
+INSERT INTO Request(locationID, bloodTypeRequested, amountRequestedML)
+			VALUES(6, 'A+', 470)
+INSERT INTO Request(locationID, bloodTypeRequested, amountRequestedML)
+			VALUES(3, 'O+', 400)
+INSERT INTO Request(locationID, bloodTypeRequested, amountRequestedML)
+			VALUES(7, 'AB+', 12)
+INSERT INTO Request(locationID, bloodTypeRequested, amountRequestedML)
+			VALUES(5, 'B+', 99)
+INSERT INTO Request(locationID, bloodTypeRequested, amountRequestedML)
+			VALUES(4, 'A-', 3090)
+INSERT INTO Request(locationID, bloodTypeRequested, amountRequestedML)
+			VALUES(3, 'O-', 31)
+INSERT INTO Request(locationID, bloodTypeRequested, amountRequestedML)
+			VALUES(5, 'B-', 99)
+INSERT INTO Request(locationID, bloodTypeRequested, amountRequestedML)
+			VALUES(6, 'AB-', 3090)
+GO
