f2f458985d52b46c27b73565bcf42c89d330b271
Natasha <natasha@bbd.co.za>
2022-02-23 15:36:48 +0200

    Added table data

---
+++ Data/Data.sql
@@ -0,0 +1,49 @@
+USE DonoraDB;
+GO
+
+INSERT INTO Transfusion(personID, amountReceivedML)
+VALUES  (4, 400),
+		(5, 470)
+GO
+
+INSERT INTO PreTest(
+	hemoglobin, 
+	temperature, 
+	bloodPressure,
+	heartPulseBPM
+)
+
+VALUES  (12.9, 36.4, '120/200', 77),
+		(14.2,35.3, '90/60', 82),
+		(13.8,37.0, '100/80', 70),
+		(13.5,36.0, '108/90', 65),
+		(15.1,37.1, '110/96', 68),
+		(17.1,36.2, '105/69', 79),
+		(14.1,35.8, '120/100', 80),
+		(13.7,37.3, '145/160', 73),
+		(14.0,36.8, '100/180', 84)
+GO
+
+
+
+INSERT INTO Donation(
+	personID , 
+	preTestID, 
+	donationType, 
+	amountDonatedML 
+)
+
+VALUES  (3,45,'Blood', 300),
+		(5,46,'Platelets', 200),
+		(7,47,'Plasma', 350)
+GO
+
+INSERT INTO DonationTypes(
+	donationType, 
+	donationInterval
+)
+
+VALUES  ('Blood', 56),
+		('Platelets', 7),
+		('Plasma', 28)
+GO
--- Tables/bd_tables.sql
+++ Tables/bd_tables.sql
@@ -1,12 +1,12 @@
 Use DonorTestingDB;
 GO
 
-CREATE TABLE [dbo].[Transfusion](
-	[transfusionID] [int] IDENTITY (1,1) NOT NULL,
-	[personID] [int] NULL, --FK
-	[pretestID] [int] NULL, --FK
-	[amountReceivedML] [int] NULL,
-	CONSTRAINT [PK_Transfusion] PRIMARY KEY CLUSTERED ([transfusionID] ASC)
+CREATE TABLE Transfusion(
+	[transfusionID] INT IDENTITY (1,1) NOT NULL,
+	[personID] INT NULL, --FK
+	[pretestID] INT NULL, --FK
+	[amountReceivedML] INT NULL,
+	CONSTRAINT [PK_Transfusion] PRIMARY KEY  ([transfusionID] ASC)
 );
 GO
 
@@ -23,16 +23,18 @@ CREATE TABLE [dbo].[Pretest](
 	[bloodPressure] [float] NULL,
 	[heartPulseBPM] [int] NULL,
 	CONSTRAINT [PK_Pretest] PRIMARY KEY CLUSTERED ([pretestID] ASC)
+	CONSTRAINT CHK_Pretest_temperature CHECK(temperature <= 37.9)
+	CONSTRAINT CHK_Pretest_personID CHECK(
 );
 GO
 
-CREATE TABLE [dbo].[Donation](
-	[donationID] [int] IDENTITY (1,1) NOT NULL,
-	[personID] [int] NULL, --FK
-	[pretestID] [int] NULL, --FK
-	[donationType] [varchar](130) NULL, --FK
-	[amountDonatedML] [int] NULL,
-	CONSTRAINT [PK_Donation] PRIMARY KEY CLUSTERED ([donationID] ASC)
+CREATE TABLE Donation(
+	donationID INT IDENTITY (1,1) NOT NULL,
+	personID INT NULL,
+	pretestID INT NULL,
+	donationType VARCHAR(130) NULL, 
+	amountDonatedML INT NULL,
+	CONSTRAINT [PK_Donation] PRIMARY KEY ([donationID] ASC)
 );
 GO
 
@@ -43,14 +45,14 @@ ALTER TABLE dbo.Donation WITH NOCHECK ADD
 	CONSTRAINT FK_Donation_DonationTypes_donationType FOREIGN KEY(donationType) REFERENCES DonationTypes(donationType)
 GO
 
-CREATE TABLE [dbo].[DonationTypes](
-	[donationType] [varchar](130) UNIQUE NOT NULL,
-	[donationInterval] [int] NULL,
+CREATE TABLE DonationTypes (
+	donationType varchar(130) UNIQUE NOT NULL,
+	donationInterval int NULL,
 	CONSTRAINT [PK_DonationTypes] PRIMARY KEY NONCLUSTERED ([donationType] ASC)
-  --CONSTRAINT unq_DonationTypes_donationType UNIQUE(donationType) 
+  
 );
 GO
-
+--CONSTRAINT unq_DonationTypes_donationType UNIQUE(donationType) 
 --DonationType is classified as a string primary key on the ERD hence it became a unique value for the DonationType table
 --Having varchar as a primary key slows down operations when querying the db so its debatable whether we keep it as a primary key or just make it a unique constraint
 --Check here for ref https://dba.stackexchange.com/questions/80806/varchar-primary-key-mysql
