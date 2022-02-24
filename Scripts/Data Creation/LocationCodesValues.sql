8f26565b7eb996ffcb255a18e06a27ead8dbb266
Mpho Futjane <mpho.futjane@vumatel.co.za>
2022-02-24 15:19:26 +0200

    Deleted Scripts table

--- Scripts/Data Creation/LocationCodesValues.sql
+++
@@ -1,9 +0,0 @@
-USE DonoraDB
-GO
-
-INSERT INTO LocationCodes(codeID, [description]) VALUES('BDRIVE', 'A Rare Popup Blood Drive Location')
-INSERT INTO LocationCodes(codeID, [description]) VALUES('SANBS', 'A Fixed SANBS Blood Donor Location')
-INSERT INTO LocationCodes(codeID, [description]) VALUES('CLINIC', 'A Private/Public Clinic That Accepts Donations')
-INSERT INTO LocationCodes(codeID, [description]) VALUES('HOSPITAL', 'A Private/Public Hospital That Accepts Donations')
-INSERT INTO LocationCodes(codeID, [description]) VALUES('ORG', 'A School/Office With Regular Blood Drives')
-GO
--- Scripts/Data Creation/LocationsValues.sql
+++
@@ -1,9 +0,0 @@
-USE DonoraDB
-GO
-
-INSERT INTO Locations(locationName, codeID, city) VALUES('BBD JHB Office', 'ORG', 'Killarney')
-INSERT INTO Locations(locationName, codeID, city) VALUES('Nelson Mandela Square', 'BDRIVE', 'Sandton')
-INSERT INTO Locations(locationName, codeID, city) VALUES('Chris Hani Baragwanath Academic Hospital', 'HOSPITAL', 'Soweto')
-INSERT INTO Locations(locationName, codeID, city) VALUES('Musgrave Donor Centre', 'SANBS', 'Durban')
-INSERT INTO Locations(locationName, codeID, city) VALUES('Claremont Clinic', 'CLINIC', 'Claremont')
-GO
