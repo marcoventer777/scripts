7b897e153cf68b6650b0ea3ca34729bfb1c43b49
Marco <marcov@bbd.co.za>
2022-02-24 14:29:16 +0200

    modified Persons, Patient, Donor and Medical Aid tables to include update and delete cascades

--- TableCreation.sql
+++ TableCreation.sql
@@ -1,31 +1,38 @@
 USE DonoraDB
 GO
 
--- CREATE TABLE Persons (
--- personID [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
--- firstName [varchar] (120) NOT NULL,
--- lastName [varchar] (120) NOT NULL,
--- weightKGS [int] NOT NULL,
--- idNum [varchar] (13) NOT NULL,
--- bloodType [varchar] (3) NOT NULL,
--- gender VARCHAR (6) NOT NULL,
--- --validation
--- UNIQUE (idNum),
--- CONSTRAINT CHK_bloodType CHECK (bloodType='A+' OR
--- bloodType='A-' OR
--- bloodType='B+' OR
--- bloodType='B-' OR
--- bloodType='AB+' OR
--- bloodType='AB-' OR
--- bloodType='O+' OR
--- bloodType='O-'),
--- CONSTRAINT CHK_weight CHECK (weightKGS >= 5), --lightest recorded adult (2.1 kg) at the age of 17.
--- CONSTRAINT CHK_gender CHECK (gender = 'male' or gender = 'female')
--- );
--- GO
+drop table Persons;
+ CREATE TABLE Persons (
+ personID int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+ firstName VARCHAR (120) NOT NULL,
+ lastName VARCHAR (120) NOT NULL,
+ weightKGS int NOT NULL,
+ idNum CHAR (13) NOT NULL,
+ bloodType VARCHAR (3) NOT NULL,
+ gender VARCHAR (6) NOT NULL,
+ mobileNum CHAR (10) NOT NULL,
+ emailAddress VARCHAR (120) NOT NULL,
+  --validation
+ UNIQUE (idNum),
+ CONSTRAINT CHK_bloodType CHECK (bloodType='A+' OR
+ bloodType='A-' OR
+ bloodType='B+' OR
+ bloodType='B-' OR
+ bloodType='AB+' OR
+ bloodType='AB-' OR
+ bloodType='O+' OR
+ bloodType='O-'),
+ CONSTRAINT CHK_weight CHECK (weightKGS >= 5), --lightest recorded adult (2.1 kg) at the age of 17.
+ CONSTRAINT CHK_gender CHECK (gender = 'male' or gender = 'female'),
+ CONSTRAINT CHK_emailRegex CHECK (emailAddress LIKE '%___@___%'), --email regex
+ CONSTRAINT CHK_mobileNUm CHECK (mobileNum NOT LIKE '%[^0-9]%' AND mobileNum LIKE '0%')
+ );
+ GO
 
 -- CREATE TABLE Patient (
--- personID int NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES Persons(personID),
+-- personID int NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES Persons(personID) 
+   ON DELETE CASCADE
+   ON UPDATE CASCADE,
 -- needStatus [varchar] (4) NOT NULL,
 -- --validation
 -- CONSTRAINT CHK_needStatus CHECK (needStatus='low' OR needStatus='high'),
@@ -42,10 +49,16 @@ GO
 -- GO
 
 -- CREATE TABLE Donor (
--- personID int NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES Persons(personID),
--- schemeCode varchar(50) NULL,
+-- personID int NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES Persons(personID)
+   ON DELETE CASCADE
+   ON UPDATE CASCADE,
+-- schemeCode varchar(50) NULL
+   ON DELETE CASCADE
+   ON UPDATE CASCADE,
 -- nextSafeDonationDate DATE NOT NULL,
 -- FOREIGN KEY (schemeCode) REFERENCES MedicalAid(schemeCode),
+-- validation that dates in past cannot be entered (because donor enrolled on day of donation)
+    CONSTRAINT CHK_dateCorrect CHECK (nextSafeDonationDate >= GETDATE())
 -- );
 -- GO
 
