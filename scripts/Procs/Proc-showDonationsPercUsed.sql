cbf646ec41c5afbd1f32dc6462dbeda63ce7b9b6
Marco Venter <98101709+marcoventer777@users.noreply.github.com>
2022-02-25 10:56:27 +0200

    UPDATED COLLATION

--- scripts/Procs/Proc-showDonationsPercUsed.sql
+++ scripts/Procs/Proc-showDonationsPercUsed.sql
@@ -1,21 +1,21 @@
 --PROC - show % of blood donated that is used in transfusions for donor.
-create or alter procedure ShowTransfusionsForDonor @donorID int 
-as
-select  p.personID, SUM(d.amountDonatedML) as TotalDonated, 
+CREATE OR ALTER PROCEDURE ShowTransfusionsForDonor @donorID INT 
+AS
+SELECT  p.personID, SUM(d.amountDonatedML) as TotalDonated, 
 	    SUM(t.amountReceivedML) as TotalUsed, 
 		[dbo].Perc_TransfusedOfDonation(SUM(d.amountDonatedML), SUM(t.amountReceivedML)) as PercUsed
-from Donor p
-left join Donation d on p.personID = d.personID
-left join Donations ds on d.donationID = ds.donationID 
-left join BloodBag b on b.bloodBagID = ds.bloodBagID
-left join Transfusions ts on ts.bloodBagID = b.bloodBagID
-left join Transfusion t on ts.transfusionID=t.transfusionID
-where p.personID = @donorID
-group by p.personID;
+FROM Donor p
+LEFT JOIN Donation d ON p.personID = d.personID
+LEFT JOIN Donations ds ON d.donationID = ds.donationID 
+LEFT JOIN BloodBag b ON b.bloodBagID = ds.bloodBagID
+LEFT JOIN Transfusions ts ON ts.bloodBagID = b.bloodBagID
+LEFT JOIN Transfusion t on ts.transfusionID=t.transfusionID
+WHERE p.personID = @donorID
+GROUP BY p.personID;
 GO
 
 --FUNC - get % used in transfusions if blood donated is given. Test: select [dbo].Perc_TransfusedOfDonation(1880, 820) as PercUsed;
-create or alter function [dbo].Perc_TransfusedOfDonation (
+CREATE OR ALTER FUNCTION [dbo].Perc_TransfusedOfDonation (
 	@donated real,
 	@transfused real
 )
